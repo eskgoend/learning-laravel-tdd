b20746fbc4170559e6f52b57c3edd061fcb4092a
o64.kaneyama.eisuke <o64.kaneyama.eisuke@gmail.com>
2022-05-02 17:51:41 +0900

    3-4 課題前

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware('auth')->group(function () {
     Route::get('/lessons/{lesson}', 'LessonController@show')->name('lessons.show');
+    Route::post('lessons/{lesson}/reserve', 'Lesson\ReserveController')->name('lessons.reserve');
 });
---
+++ tests/Feature/Http/Controllers/Lesson/ReserveControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Lesson;
+
+use App\Models\Lesson;
+use App\Models\Reservation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\Factories\Traits\CreatesUser;
+use Tests\TestCase;
+
+class ReserveControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use CreatesUser;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testInvoke_正常系()
+    {
+        $lesson = Lesson::factory()->create();
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        $response = $this->post("/lessons/{$lesson->id}/reserve");
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect("/lessons/{$lesson->id}");
+
+        $this->assertDatabaseHas('reservations', [
+            'lesson_id' => $lesson->id,
+            'user_id' => $user->id,
+        ]);
+    }
+
+    public function testInvoke_異常系()
+    {
+        $lesson = Lesson::factory()->create(['capacity' => 1]);
+        $anotherUser = $this->createUser();
+        $lesson->reservations()->save(Reservation::factory()->make(['user_id' => $anotherUser]));
+
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        $response = $this->from("lessons/{$lesson->id}")
+                        ->post("/lessons/{$lesson->id}/reserve");
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect("/lessons/{$lesson->id}");
+        $response->assertSessionHasErrors();
+
+        $error = session('errors')->first();
+        $this->assertStringContainsString('予約できません。', $error);
+
+        $this->assertDatabaseMissing('reservations', [
+            'lesson_id' => $lesson->id,
+            'user_id' => $user->id,
+        ]);
+    }
+}
